afb0c7ae887530bc4597fc00e71b9b7d3d40a209
Jhonatan <jhonatanb.s4nt0s@gmail.com>
2021-11-02 12:22:28 -0300

    Foi adicionado o efeito de fechar e abrir o menu na página com JavaScript.

--- ProjetoInfusion/js/main.js
+++ ProjetoInfusion/js/main.js
@@ -3,6 +3,9 @@
     $body.classList.remove("no-js");
     $body.classList.add("js");
 
-    var $btnMenu = document.querySelector(".header__btnMenu");
-    $btnMenu.removeAttribute("style");
+    var menu = new Menu({
+        container: ".header__nav",
+        toggleBtn: ".header__btnMenu",
+        widthEnable:1024
+    })
 })()
