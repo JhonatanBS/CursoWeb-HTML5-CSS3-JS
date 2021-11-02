573f909299026c8fb2d56262fbc9db1b97d92666
Jhonatan <jhonatanb.s4nt0s@gmail.com>
2021-11-02 10:36:19 -0300

    Usando Javascript , o botão apareceu e foi personalizado no CSS e no HTML.

--- ProjetoInfusion/js/main.js
+++ ProjetoInfusion/js/main.js
@@ -2,4 +2,7 @@
     var $body = document.querySelector("body");
     $body.classList.remove("no-js");
     $body.classList.add("js");
+
+    var $btnMenu = document.querySelector(".header__btnMenu");
+    $btnMenu.removeAttribute("style");
 })()
